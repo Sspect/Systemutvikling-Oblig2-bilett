463e477b51adeec9a940c2cadeb65505d2a7242f
Sspect <bastian.stokke@gmail.com>
2023-10-29 23:29:51 +0100

    database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+
+
+--SELECT 1;
+INSERT INTO bilett (film, fornavn, etternavn, epost, tellefonnummer, antall, seter) VALUES ( 'Inception','Ole','Oleson','Ole.Oleson@omail.ole','11111111','1','1A');
+INSERT INTO bilett (film, fornavn, etternavn, epost, tellefonnummer, antall, seter) VALUES ( 'Toy Story 3','Inger','Ingerson','Inger.Ingerson@Imail.Inger','22222222','2','1B,2B');
+INSERT INTO bilett (film, fornavn, etternavn, epost, tellefonnummer, antall, seter) VALUES ( '127 Hours','Per','Person','Per.Person@Pmail.Per','33333333','3','1C,2C,3C');
+INSERT INTO bilett (film, fornavn, etternavn, epost, tellefonnummer, antall, seter) VALUES ( 'Despicable Me','Mari','Marison','Mari.Marison@Mmail.Mari','444444444','4','1D,2D,3D,4D');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE bilett (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        film VARCHAR(255) NOT NULL,
+                        seter VARCHAR(255) NOT NULL,
+                        fornavn VARCHAR(255) NOT NULL,
+                        etternavn VARCHAR(255) NOT NULL,
+                        epost VARCHAR(255) NOT NULL,
+                        tellefonnummer INT NOT NULL,
+                        antall INT NOT NULL
+
+
+);
